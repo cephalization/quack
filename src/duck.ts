d5677924c33656f8a7fc9f3f190d180851f1b229
Tony Powell <apowell@arize.com>
2024-10-04 21:59:05 -0400

    Serve duckdb wasm from cdn in production

--- src/duck.ts
+++ src/duck.ts
@@ -1,24 +1,53 @@
 import * as duckdb from "@duckdb/duckdb-wasm";
-import duckdb_wasm from "@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm?url";
-import mvp_worker from "@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js?url";
-import duckdb_wasm_eh from "@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url";
-import eh_worker from "@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url";
 
-const MANUAL_BUNDLES: duckdb.DuckDBBundles = {
-  mvp: {
-    mainModule: duckdb_wasm,
-    mainWorker: mvp_worker,
-  },
-  eh: {
-    mainModule: duckdb_wasm_eh,
-    mainWorker: eh_worker,
-  },
-};
+const production = import.meta.env.PROD;
 
+const makeUrl = (path: string) =>
+  `https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm/dist/${path}`;
+
+const MANUAL_BUNDLES:
+  | duckdb.DuckDBBundles
+  | (() => Promise<duckdb.DuckDBBundles>) = production
+  ? {
+      mvp: {
+        mainModule: makeUrl("duckdb-mvp.wasm"),
+        mainWorker: makeUrl("duckdb-browser-mvp.worker.js"),
+      },
+      eh: {
+        mainModule: makeUrl("duckdb-eh.wasm"),
+        mainWorker: makeUrl("duckdb-browser-eh.worker.js"),
+      },
+    }
+  : async () => ({
+      mvp: {
+        mainModule: (
+          await import("@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm?url")
+        ).default,
+        mainWorker: (
+          await import(
+            "@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js?url"
+          )
+        ).default,
+      },
+      eh: {
+        mainModule: (
+          await import("@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url")
+        ).default,
+        mainWorker: (
+          await import(
+            "@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url"
+          )
+        ).default,
+      },
+    });
 // https://duckdb.org/docs/api/wasm/instantiation#vite
 export const createDb = async () => {
+  const bundles =
+    typeof MANUAL_BUNDLES === "function"
+      ? await MANUAL_BUNDLES()
+      : MANUAL_BUNDLES;
   // Select a bundle based on browser checks
-  const bundle = await duckdb.selectBundle(MANUAL_BUNDLES);
+  const bundle = await duckdb.selectBundle(bundles);
   // Instantiate the asynchronus version of DuckDB-wasm
   const worker = new Worker(bundle.mainWorker!);
   const logger = new duckdb.ConsoleLogger();
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [react()],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "src"),
     },
   },
-});
+}));
